826b4308ddfbfef5cbb0d32606c49572b8e580bd
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-06-19 19:30:51 -0300

    basic bird class

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
 import express from 'express'
+import {Bird} from './bird.js'
+
 const APP_PATH: string = "/"
 
 const app = express()
 
+const birds = [
+    new Bird(
+        'Cotorra Argentina',
+        'Myiopsitta monachus',
+        'Es la especie del cotorro Tony, la mascota de Noah.'
+    ),
+    new Bird(
+        'Cotorra Australiana',
+        'Melopsittacus undulatus',
+        'Es un ave de jaula muy popular, no solo por sus vivos colores sino también por su fácil adaptación.'
+    ),
+]
+
 app.use(APP_PATH, (req, res) => 
 {
-    res.send("HOOLAAAAAA :P")
+    res.send(birds)
 })
 
 app.listen(3000, () =>{
