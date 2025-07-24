280127d5192a941191782c32f7db03556ea222a5
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-07-24 12:59:05 -0300

    Update auth.ts

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -7,27 +7,32 @@ config();
 const router = Router();
 
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: "http://localhost:3000/api/auth/google/callback",
-    },
-    function (accessToken, refreshToken, profile, cb) {
-      //find or create user in db
-    }
-  )
+	new GoogleStrategy(
+		{
+			clientID: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+			callbackURL: "http://localhost:3000/api/auth/google/callback",
+			passReqToCallback: true,
+		},
+		function (accessToken, refreshToken, profile, cb) {
+			//find or create user in db
+		}
+	)
 );
 
 router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 
 router.get(
-  "/google/callback",
-  passport.authenticate("google", { failureRedirect: "/login" }),
-  function (req, res) {
-    // Successful authentication, redirect home.
-    res.redirect("/");
-  }
+	"/google/callback",
+	passport.authenticate("google", { failureRedirect: "/err" }),
+	function (req, res) {
+		// Successful authentication, redirect home.
+		console.log("User authenticated:", req.user);
+		res.redirect("http://localhost:5173");
+	}
 );
 
+router.get("/err", (req, res) => {
+	res.status(401).json({ message: "Authentication failed" });
+});
 export default router;
