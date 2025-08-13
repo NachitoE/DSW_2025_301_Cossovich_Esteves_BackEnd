008aa25f9115e6b06e5c52fe3b1851cdedfbd910
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-08-13 12:28:54 -0300

    creating comment system

--- src/routes/comments.ts
+++ src/routes/comments.ts
@@ -6,33 +6,34 @@ const router = Router();
 
 // get IComment body
 router.post("/", async (req, res) => {
-	const { text, birdId, userId } = req.body;
+  const { text, birdId, userId } = req.body;
 
-	if (!text || !birdId || !userId) {
-		return res.status(400).json({ msg: "Faltan campos obligatorios" });
-	}
+  if (!text || !birdId || !userId) {
+    return res.status(400).json({ msg: "Faltan campos obligatorios" });
+  }
 
-	const comment = new Comment();
-	comment.text = text;
-	comment.birdId = birdId;
-	comment.userId = userId;
-	comment.createdAt = new Date();
+  const createdComment = await req.em.create(Comment, {
+    text,
+    birdId,
+    userId,
+    createdAt: new Date(),
+  });
 
-	await req.em.persistAndFlush(comment);
-	res.status(201).json(comment);
+  await req.em.persistAndFlush(createdComment);
+  res.status(201).json(createdComment);
 });
 
 // get all comments given a birdId
 // TODO: maybe validate if bird exists
 router.get("/:birdId", async (req, res) => {
-	const { birdId } = req.params;
-	const comments = await req.em.find(Comment, { birdId: birdId });
-	if (!comments) {
-		res
-			.status(404)
-			.json({ msg: "No comments found for this bird OR bird not existing" });
-	}
-	res.status(200).json(comments);
+  const { birdId } = req.params;
+  const comments = await req.em.find(Comment, { birdId: birdId });
+  if (!comments) {
+    res
+      .status(404)
+      .json({ msg: "No comments found for this bird OR bird not existing" });
+  }
+  res.status(200).json(comments);
 });
 
 export default router;
