eb5bea7274e7fc386b8bfaccf8fe4895a3e3631a
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-08-15 17:59:11 -0300

    fixed bird post endpoint

--- src/routes/birds.ts
+++ src/routes/birds.ts
@@ -7,69 +7,69 @@ import { ObjectId } from "@mikro-orm/mongodb";
 const router = Router();
 
 router.get("/", async (req, res) => {
-  const birds = await req.em.findAll(Bird);
-  res.json({ data: birds });
+	const birds = await req.em.findAll(Bird);
+	res.json({ data: birds });
 });
 
 router.get(`${"/"}:id`, async (req, res) => {
-  console.log(req.params.id);
-  const bird = await req.em.findOne(Bird, { _id: new ObjectId(req.params.id) });
-  if (!bird) {
-    res.status(404).send({ message: "bird not found" });
-    return;
-  }
-  res.json({ data: bird });
+	console.log(req.params.id);
+	const bird = await req.em.findOne(Bird, { _id: new ObjectId(req.params.id) });
+	if (!bird) {
+		res.status(404).send({ message: "bird not found" });
+		return;
+	}
+	res.json({ data: bird });
 });
 
 router.post("/", async (req, res) => {
-  //check whether user body was sent
-  const userBody = req.body.user as IUser;
-  if (!userBody) {
-    return res.status(403).send({ message: "Non detected login." });
-  }
-  //check whether user is admin or exists
-  const user = (await req.em.findOne(User, {
-    googleId: userBody.googleId,
-  })) as User;
+	//check whether user body was sent
+	const userBody = req.body.data.user as IUser;
+	if (!userBody) {
+		return res.status(403).send({ message: "Non detected login." });
+	}
+	//check whether user is admin or exists
+	const user = (await req.em.findOne(User, {
+		googleId: userBody.googleId,
+	})) as User;
 
-  if (!user || user.role !== "admin") {
-    return res
-      .status(403)
-      .send({ message: "Only admins are allowed to create birds." });
-  }
-  const { name, scientificName, description, imageURL } = req.body.bird;
-  const createdBird = req.em.create(Bird, {
-    name,
-    scientificName,
-    description,
-    imageURL,
-  });
-  await req.em.persistAndFlush(createdBird);
-  res.status(201).send({
-    message: "Bird succesfully created.",
-    data: createdBird,
-  });
+	if (!user || user.role !== "admin") {
+		return res
+			.status(403)
+			.send({ message: "Only admins are allowed to create birds." });
+	}
+	const { name, scientificName, description, imageURL } = req.body.data.bird;
+	const createdBird = req.em.create(Bird, {
+		name,
+		scientificName,
+		description,
+		imageURL,
+	});
+	await req.em.persistAndFlush(createdBird);
+	res.status(201).send({
+		message: "Bird succesfully created.",
+		data: createdBird,
+	});
 });
 
 router.put(`${"/"}:id`, async (req, res) => {
-  const bird = await req.em.findOne(Bird, { _id: new ObjectId(req.params.id) });
-  if (!bird) {
-    res.status(404).send({ message: "bird not found" });
-    return;
-  }
-  const oldData = { ...bird };
-  const inputData = {
-    name: req.body.name,
-    scientificName: req.body.scientificName,
-    description: req.body.description,
-  };
-  const replacedBird = req.em.assign(bird, inputData);
-  await req.em.persistAndFlush(replacedBird);
-  res.status(200).send({
-    message: "Replaced bird data.",
-    data: replacedBird,
-    old_data: oldData,
-  });
+	const bird = await req.em.findOne(Bird, { _id: new ObjectId(req.params.id) });
+	if (!bird) {
+		res.status(404).send({ message: "bird not found" });
+		return;
+	}
+	const oldData = { ...bird };
+	const inputData = {
+		name: req.body.name,
+		scientificName: req.body.scientificName,
+		description: req.body.description,
+	};
+	const replacedBird = req.em.assign(bird, inputData);
+	await req.em.persistAndFlush(replacedBird);
+	res.status(200).send({
+		message: "Replaced bird data.",
+		data: replacedBird,
+		old_data: oldData,
+	});
 });
 
 export default router;
