026e29153155d3dcc2be226305ed162e6e0c563e
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-10-03 00:36:14 -0300

    implementing jwt

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,13 @@ import cors from "cors";
 import session from "express-session";
 import passport from "passport";
 import {
-	EntityManager,
-	MikroORM,
-	MongoEntityManager,
+  EntityManager,
+  MikroORM,
+  MongoEntityManager,
 } from "@mikro-orm/mongodb";
 import mikroOrmConfig from "./mikro-orm.config.js";
 import { appConfig } from "./config/config.js";
+import { expressjwt } from "express-jwt";
 // --- Routers ---
 import { Router } from "express";
 import birdsRouter from "./routes/birds.js";
@@ -21,54 +22,72 @@ import { initBirdVisualTraits } from "./routes/birdVisualTraits.js";
 import { Services } from "./services/Services.js";
 
 const API_DICT: Record<string, Router> = {
-	[appConfig.paths.birds_path]: birdsRouter,
-	[appConfig.paths.auth_path]: authRouter,
-	[appConfig.paths.comments_path]: commentsRouter,
-	[appConfig.paths.users_path]: usersRouter,
-	[appConfig.paths.bird_visual_traits_path]: birdVisualTraitsRouter,
+  [appConfig.paths.birds_path]: birdsRouter,
+  [appConfig.paths.auth_path]: authRouter,
+  [appConfig.paths.comments_path]: commentsRouter,
+  [appConfig.paths.users_path]: usersRouter,
+  [appConfig.paths.bird_visual_traits_path]: birdVisualTraitsRouter,
 };
 
 //----- Initialize app -----
 async function main() {
-	const orm = await MikroORM.init(mikroOrmConfig);
-	console.log("MikroORM inicializado");
-	const em = orm.em.fork() as MongoEntityManager; // get entity manager
-	//----- Express -----
-	const app = express();
-	app.use(
-		cors({
-			origin: appConfig.reactAppUrl, // react
-			credentials: true,
-		})
-	);
-	app.use((req, res, next) => {
-		req.services = new Services(em); // inyect services middleware
-		next();
-	});
-	app.use(express.json());
-	app.use(
-		session({
-			secret: process.env.SESSION_SECRET!,
-			resave: false,
-			saveUninitialized: false,
-		})
-	);
-	//----- Passport -----
-	app.use(passport.initialize());
-	app.use(passport.session());
-	//----- Initialize DB -----
-	initializeDatabase(em);
-	//----- Assign Routers -----
-	Object.entries(API_DICT).forEach(([path, router]) => {
-		app.use(path, router);
-	});
-	app.listen(appConfig.port, () => {
-		console.log(`Listening at ${appConfig.apiBaseUrl}:${appConfig.port}`);
-	});
+  const orm = await MikroORM.init(mikroOrmConfig);
+  console.log("MikroORM inicializado");
+  const em = orm.em.fork() as MongoEntityManager; // get entity manager
+  //----- Express -----
+  const app = express();
+  app.use(
+    cors({
+      origin: appConfig.reactAppUrl, // react
+      credentials: true,
+    })
+  );
+  app.use((req, res, next) => {
+    req.services = new Services(em); // inyect services middleware
+    next();
+  });
+  app.use(express.json());
+  app.use(
+    expressjwt({
+      secret: process.env.JWT_SECRET!,
+      algorithms: ["HS256"],
+      credentialsRequired: false,
+      getToken: function fromHeaderOrQuerystring(req) {
+        if (
+          req.headers.authorization &&
+          req.headers.authorization.split(" ")[0] === "Bearer"
+        ) {
+          return req.headers.authorization.split(" ")[1];
+        } else if (req.query && req.query.token) {
+          return req.query.token as string;
+        }
+        return undefined;
+      },
+    })
+  );
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET!,
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+  //----- Passport -----
+  app.use(passport.initialize());
+  app.use(passport.session());
+  //----- Initialize DB -----
+  initializeDatabase(em);
+  //----- Assign Routers -----
+  Object.entries(API_DICT).forEach(([path, router]) => {
+    app.use(path, router);
+  });
+  app.listen(appConfig.port, () => {
+    console.log(`Listening at ${appConfig.apiBaseUrl}:${appConfig.port}`);
+  });
 }
 
 function initializeDatabase(em: EntityManager) {
-	initBirdVisualTraits(new Services(em).birdVisualTrait);
+  initBirdVisualTraits(new Services(em).birdVisualTrait);
 }
 
 main();
